ec724c9a2f81c583a768e649f44f3f734720a84c
Hassan <blaugrana888@hotmail.com>
2023-11-27 12:59:52 -0800

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-'''1. Real definition of a rectangle'''
+"""1. Real definition of a rectangle."""
 
 
 class Rectangle:
-    '''Class Rectangle that defines a rectangle'''
+    """Class Rectangle that defines a rectangle."""
 
     def __init__(self, width=0, height=0):
-        '''init the rectangle'''
+        """Init the rectangle."""
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        '''getter for private attribute'''
+        """Getter for private attribute."""
         return self.__width
     
     @width.setter
     def width(self, value):
-        '''setter for private attribute'''
+        """Setter for private attribute."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = value 
 
     @property
     def highet(self):
-        '''getter for private attribute'''
+        """Getter for private attribute."""
         return self.__highet
     
     @highet.setter
     def highet(self, value):
-        '''setter for private attribute'''
+        """Setter for private attribute."""
         if type(value) is not int:
             raise TypeError("highet must be an integer")
         if value < 0:
             raise ValueError("highet must be >= 0")
-        self.__highet = value
+        self.__highet = value 
