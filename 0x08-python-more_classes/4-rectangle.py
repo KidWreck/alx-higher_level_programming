3ba38f7064a2139c1610e8e5950270b7e6618b4f
Hassan <blaugrana888@hotmail.com>
2023-11-27 14:15:19 -0800

    4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""4. Eval is magic."""
+
+
+class Rectangle:
+    """Class Rectangle that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Init the rectangle."""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Getter for private attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for private attribute."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for private attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for private attribute."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate the area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate the perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Return a string of '#' rectangle."""
+        s = ""
+        if self.width != 0 and self.height != 0:
+            s += "/n".join("#" * self.__width for i in range(self.__height))
+        return s
+
+    def __repr__(self):
+        """Return a represintaition of '#' rectangle."""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
