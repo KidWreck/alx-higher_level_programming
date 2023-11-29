f0d93aed1ae898b6953e0920eb6423b19797855c
Hassan <blaugrana888@hotmail.com>
2023-11-28 16:06:06 -0800

    advance

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.h = getattr(magic_string, 'h', 0) + 1
-    return ("Best School, " * (magic_string.h - 1) + "Best School")
+    return ", ".join(["Best School" for i in range(magic_string.h)]
