987c7bd7eb32ae79ef3cfea0dd0ca6203840ad97
Hassan <blaugrana888@hotmail.com>
2023-12-04 14:00:59 -0800

    rect

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''8. Rectangle.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Subclass for rectangle.'''
+
+    def __init__(self, width, height):
+        '''Init.'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''9. Full rectangle.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Subclass for rectangle.'''
+
+    def __init__(self, width, height):
+        '''Init.'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''Area of rectangle.'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''String representation.'''
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
