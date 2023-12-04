d7d21d3035894f1fc2271958e7926b938fef902c
Hassan <blaugrana888@hotmail.com>
2023-12-04 13:32:37 -0800

    2,3,4

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''2. Exact same object.'''
+
+
+def is_same_class(obj, a_class):
+    '''Determines if an object is exactly an instance of a class.'''
+    return type(obj) == a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''3. Same class or inherit from.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Determines if an object is a subclass of a class.'''
+    return isinstance(obj, a_class)
